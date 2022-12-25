637cac1b6737cfca05b0458ca0375245f817f330
Alyssa Sitto <alyssasitto1@gmail.com>
2022-12-24 23:58:31 -0800

    Add home page and universal styles

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,20 @@
+require("./Home.css");
+
+function Home() {
+	return (
+		<div className="home-page">
+			<header>
+				<img src="images/header-img.png"></img>
+				<h1>Hello I'm Alyssa</h1>
+				<p>
+					I'm an aspiring web developer from southern California with a passion
+					for coding. I turn my ideas into user-friendly and aesthetically
+					pleasing web applications with a goal of providing the best possible
+					service. Check out my work below to get an idea of who I am.
+				</p>
+			</header>
+		</div>
+	);
+}
+
+export default Home;
